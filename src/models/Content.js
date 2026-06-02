1298d2b4533755b170bcd4be903e52dd87496158
ninatrbojevic <nina.trbojevic@student.uniri.hr>
2026-06-02 20:47:12 +0200

    changes in Component.js

--- src/models/Content.js
+++ src/models/Content.js
@@ -1,22 +1,35 @@
 const mongoose = require('mongoose');
 
 const contentSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-    default: 'u_obradi'
-  }
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['u_obradi', 'odobren', 'odbijen'],
+        default: 'u_obradi'
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    request_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Request',
+        required: true,
+    },
+    url: {
+        type: String,
+    },
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 const Content = mongoose.model('Content', contentSchema);

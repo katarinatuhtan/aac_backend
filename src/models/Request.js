7b9bb057f4c17cf5042bbda0ee01a5c56a921a5f
ninatrbojevic <nina.trbojevic@student.uniri.hr>
2026-06-03 21:30:00 +0200

    changes in request schema

--- src/models/Request.js
+++ src/models/Request.js
@@ -1,28 +1,38 @@
 const mongoose = require('mongoose');
 
 const requestSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  last_name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  status: {
-    type: String,
-    required: true,
-    default: 'zaprimljeno'
-  }
-//   user_id: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true
-//   }
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    impairment: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'zaprimljeno'
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 const Request = mongoose.model('Request', requestSchema);
